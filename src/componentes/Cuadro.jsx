6997544c48d4d98651b449d4257e858a562bdc11
Samuel Argueta <arg211024@uvg.edu.gt>
2023-03-17 17:28:39 -0600

    Mas mejoras

--- src/componentes/Cuadro.jsx
+++ src/componentes/Cuadro.jsx
@@ -3,7 +3,7 @@ import './Cuadro.css'
 export default function Cuadro({tittle,image, description, link, linkdata }){
     return(
         <div className='box'>
-            `<img src={image} alt="" />
+            `<img src={image} alt="data" />
             <h3>{tittle}</h3>
             <p>{description} 
                 <a href={link}>
